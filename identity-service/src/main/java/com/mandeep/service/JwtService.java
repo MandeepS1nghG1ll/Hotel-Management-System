5f6e29e22541eb628bf91ec5a641144b98924ebb
Mandeep Singh Gill <msgilla13@gmail.com>
2025-05-12 01:07:14 +0530

    added identity service

---
+++ identity-service/src/main/java/com/mandeep/service/JwtService.java
@@ -0,0 +1,63 @@
+package com.mandeep.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import com.mandeep.entity.UserCredential;
+import com.mandeep.repository.UserCredentialRepository;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+public class JwtService {
+
+
+    @Autowired
+    UserCredentialRepository repository;
+    
+	public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+
+
+    public void validateToken(final String token) {
+        Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+    }
+
+
+    public String generateToken(String userName) {
+//        Map<String, Object> claims = new HashMap<>();
+//        return createToken(claims, userName);
+    	UserCredential user = repository.findByName(userName)
+    	        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    	    
+    	    Map<String, Object> claims = new HashMap<>();
+    	    claims.put("roles", user.getRoles()); // assuming user.getRoles() returns List<String> or comma-separated roles
+    	    return createToken(claims, userName);
+    }
+
+    private String createToken(Map<String, Object> claims, String userName) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userName)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
